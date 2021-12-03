f1e17f42f123e17a66909bc2e83b930da1b15820
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2021-12-03 21:51:08 +0800

    init

---
+++ app/src/main/java/com/example/drawerdemo/h5utils/JsMethods.kt
@@ -0,0 +1,27 @@
+package com.example.drawerdemo.h5utils
+
+import android.content.Context
+import android.webkit.JavascriptInterface
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar_layout.*
+
+// Kotlin与H5通讯的桥梁类
+class JsMethods(context:Context) {
+
+    // 上下们对象
+    private var mContext = context
+
+    @JavascriptInterface // 安卓4.2之后都要加
+    fun shouToast(json:String) {
+        // 第一种方式弹出
+//        Toast.makeText(mContext, json, Toast.LENGTH_SHORT).show()
+        println("获得的网页源代码是:")
+        println(json)
+    }
+
+    @JavascriptInterface // 安卓4.2之后都要加
+    fun getHome() {
+    }
+
+}
